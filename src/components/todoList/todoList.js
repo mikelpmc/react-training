0623cea53dd071f773b4148a0a68bfe595fb5d34
Mikel <mikel.parra@appspace.com>
2022-06-23 15:28:59 +0200

    feat: add basic counter state

--- src/components/todoList/todoList.js
+++ src/components/todoList/todoList.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoList = ({ username = 'No user', children, ...rest }) => {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div {...rest}>
-            <h1>{username} ToDo List</h1>
+            <h1>
+                {username} ToDo List - {count}
+            </h1>
+            <button type="button" onClick={handleClick}>
+                Add ToDo
+            </button>
             {children}
         </div>
     );
