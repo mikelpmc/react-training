00525d33436697f3816170da13589af0e70586dd
Christian Ruiz <christian.ruiz@beezy.net>
2022-06-26 19:23:22 +0800

    feat: styling the list items

---
+++ src/components/todoItem/todoItem.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledItem = styled.li`
+	max-width: 300px;
+	padding: 20px;
+	margin: 0 auto;
+
+	&:hover {
+		background-color: lightgrey;
+	}
+`;
--- src/components/todoItem/todoItem.tsx
+++ src/components/todoItem/todoItem.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import styles from './todoItem.module.css';
+import { StyledItem } from './todoItem.style';
 
 const TodoItem = ({ title }: { title: string }): JSX.Element => {
 	const [isDone, setIsDone] = useState(false);
 
 	return (
-		<li className={isDone ? styles['item--done'] : ''} onClick={() => setIsDone(!isDone)}>
+		<StyledItem className={isDone ? styles['item--done'] : ''} onClick={() => setIsDone(!isDone)}>
 			{title}
-		</li>
+		</StyledItem>
 	);
 };
 
