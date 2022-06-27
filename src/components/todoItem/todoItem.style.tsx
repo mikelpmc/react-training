317af7c65298907fcaf55b04eb5fb2a0ecdb9589
Christian Ruiz <christian.ruiz@beezy.net>
2022-06-27 15:01:22 +0800

    feat: using polished functions

--- src/components/todoItem/todoItem.style.tsx
+++ src/components/todoItem/todoItem.style.tsx
@@ -1,16 +1,17 @@
+import { rem, darken } from 'polished';
 import styled from 'styled-components';
 
 export const StyledItem = styled.li<{
 	$isDone: boolean;
 }>`
-	max-width: 300px;
-	padding: 20px;
+	max-width: ${rem(300)};
+	padding: ${rem(20)};
 	margin: 0 auto;
 
 	${({ $isDone }) => $isDone && 'text-decoration: line-through'};
 
 	&:hover {
-		background-color: lightgrey;
+		background-color: ${darken(0.5, 'lightgrey')};
 	}
 `;
 
