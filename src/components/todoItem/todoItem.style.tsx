5eb94c4c4bcff45c76c311909a922c5bb3decc2e
Christian Ruiz <christian.ruiz@beezy.net>
2022-06-27 14:40:16 +0800

    feat: using conditional based on props on styled components

--- src/components/todoItem/todoItem.style.tsx
+++ src/components/todoItem/todoItem.style.tsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
-export const StyledItem = styled.li`
+export const StyledItem = styled.li<{
+	$isDone: boolean;
+}>`
 	max-width: 300px;
 	padding: 20px;
 	margin: 0 auto;
+	text-decoration: ${({ $isDone }) => ($isDone ? 'line-through' : 'none')};
 
 	&:hover {
 		background-color: lightgrey;
--- src/components/todoItem/todoItem.tsx
+++ src/components/todoItem/todoItem.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import styles from './todoItem.module.css';
 import { StyledItem } from './todoItem.style';
 
 const TodoItem = ({ title }: { title: string }): JSX.Element => {
 	const [isDone, setIsDone] = useState(false);
 
 	return (
-		<StyledItem className={isDone ? styles['item--done'] : ''} onClick={() => setIsDone(!isDone)}>
+		<StyledItem $isDone={isDone} onClick={() => setIsDone(!isDone)}>
 			{title}
 		</StyledItem>
 	);
